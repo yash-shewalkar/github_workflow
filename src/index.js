12dd0eb4124a4d4c06db29095d0e7fcb35dd8558
yash-shewalkar <iamyashshewalkar@gmail.com>
2025-04-28 10:58:16 +0530

    second

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ function add(a, b) {
     return a + b;
   }
   
-  module.exports = { add };
+function divide(a, b) {
+    if (b === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new TypeError('Both arguments must be numbers');
+    }
+    return a / b;
+  }
+  module.exports = { add, divide };
   
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,13 @@
-const { add } = require('../src/index');
+const { add, divide } = require('../src/index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('divides 6 by 2 to equal 3', () => {
+  expect(divide(6, 2)).toBe(3);
+}   );
+
+test('divides by zero throws error', () => {
+  expect(() => divide(6, 0)).toThrow('Cannot divide by zero');
+}   );
